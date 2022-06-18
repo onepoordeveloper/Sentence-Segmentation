4294948b2444b8999ff5239c85b7453f2869b673
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2022-06-18 22:10:13 +0300

    Content script.js refactored.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,97 +2,88 @@
 var elem;
 var lineSeparator;
 var doubleSpace;
-$(document).ready(function() {
+$(document).ready(function () {
     var work = true;
     var border = false;
     lineSeparator = true;
     doubleSpace = false;
 
     //checking if switch is off
-    chrome.runtime.sendMessage({method: "getStatus"}, function(response) {
-        if (response.message == "Off"){
+    chrome.runtime.sendMessage({ method: "getStatus" }, function (response) {
+        const isModeOn = response.message !== "Off";
+        work = isModeOn;
+        if (!isModeOn)
             console.log("turned off");
-            work = false;
-			$("body").addClass("work");
-        }
     });
 
     $("body.work *").mouseenter(function (e) {
-        if (work == false) return;
+        if (!work)
+            return;
         $(e.target).addClass("hoverActive");
         $(e.target).parent().addClass("hoverActiveParent");
-        if (border == true) $(e.target).parent().addClass("border");
+        if (border)
+            $(e.target).parent().addClass("border");
 
     });
+
     $("body.work *").mouseleave(function () {
-        if (work == false) return;
+        if (!work)
+            return;
         $(".hoverActive").removeClass("hoverActive");
         $(".hoverActiveParent").removeClass("hoverActiveParent");
         $(".border").removeClass("border");
     });
 
     $("body.work").mousedown(function (e) {
-        if (work == false) return;
+        if (!work)
+            return;
         $(".hoverActiveParent").attr("id", "pickMe");
     });
 
-    chrome.runtime.sendMessage({method: "getAutoSegment"}, function(response) {
-        if (response.message == "On") {
-            setTimeout(function(){
+    chrome.runtime.sendMessage({ method: "getAutoSegment" }, function (response) {
+        if (response.message === "On") {
+            setTimeout(function () {
                 segmentSection('body');
-            },1000);
+            }, 1000);
         }
     });
 
-    chrome.runtime.sendMessage({method: "getParaBorder"}, function(response) {
-
-        if (response.message == "Off"){
+    chrome.runtime.sendMessage({ method: "getParaBorder" }, function (response) {
+        const isModeOn = response.message !== "Off";
+        border = isModeOn;
+        if (!isModeOn)
             console.log("border turned off");
-            border = false;
-        }
-        else{
-            border = true;
-        }
     });
 
-    chrome.runtime.sendMessage({method: "getDoubleSpace"}, function(response) {
-
-        if (response.message == "On"){
+    chrome.runtime.sendMessage({ method: "getDoubleSpace" }, function (response) {
+        const isModeOn = response.message === "On";
+        doubleSpace = isModeOn;
+        if (isModeOn)
             console.log("border turned On");
-            doubleSpace = true;
-        }
-        else{
-            doubleSpace = false;
-        }
     });
 
-    chrome.runtime.sendMessage({method: "getLineSeparator"}, function(response) {
-        if (response.message == "Off"){
-            lineSeparator = false;
-        }
-        else if (response.message == "On"){
-            lineSeparator = true;
-        }
+    chrome.runtime.sendMessage({ method: "getLineSeparator" }, function (response) {
+        lineSeparator = response.message === "On";
     });
 
 });
 
-var segmentSection = function(which){
-    if (which == "section") elem = "#pickMe";
-    else elem = "body";
-    $(elem + " *").each(function(){
-        var v = $(this).html();
-        var regexForPeriod = /\.\s/g;       //regex to find period
-        var regexForQuestion = /\?\s/g;     //regex to find question mark
-        var lineBreak = doubleSpace?".&nbsp;&nbsp;":".<br/>";
-        var questionBreak = "?<br/>";
-        var separatorElem = doubleSpace?"":"<span class='segmentSeparator'></span>";
-        if (lineSeparator) separatorElem = "<span class='segmentSeparator sepBorder'></span>";
+const segmentSection = function (which) {
+    elem = (which === "section") ? "#pickMe" : "body";
+    $(elem + " *").each(function () {
+        let v = $(this).html();
+        const regexForPeriod = /\.\s/g;       //regex to find period
+        const regexForQuestion = /\?\s/g;     //regex to find question mark
+        const lineBreak = doubleSpace ? ".&nbsp;&nbsp;" : ".<br/>";
+        const questionBreak = "?<br/>";
+        let separatorElem = doubleSpace ? "" : "<span class='segmentSeparator'></span>";
+        if (lineSeparator)
+            separatorElem = "<span class='segmentSeparator sepBorder'></span>";
         v = v.replace(regexForPeriod, (lineBreak + separatorElem));        //replacing period with period and newline
         v = v.replace(regexForQuestion, (questionBreak + separatorElem));      //replacing questionmark with questionmark and newline
         $(this).html(v);        //adding replaced content back to the DOM element
     });
-    $("#pickMe").attr("id","");
+    $("#pickMe").attr("id", "");
 };
 
 
